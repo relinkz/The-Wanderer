12215d3fa8f2c36f4edb60e5253b00f8f374a275
Sebastian Lundgren <relinkz@users.noreply.github.com>
2022-04-13 13:57:29 +0200

    [Cleaning] minor fixes
    Explosion from bullet follow the target it collided with.
    
    deactivate bullets that has moved from screen

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] ParticleSystem explosionFX;
-    ParticleSystem explosionFxCopy;
+	[SerializeField] ParticleSystem explosionFX;
+	ParticleSystem explosionFxCopy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        explosionFxCopy = Instantiate(explosionFX, transform.position, Quaternion.identity);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        explosionFxCopy.transform.position = transform.position;
-        explosionFxCopy.Play();
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		explosionFxCopy = Instantiate(explosionFX, collision.transform);
+		explosionFxCopy.transform.position = transform.position;
+		explosionFxCopy.Play();
 
-        // using pooling
-        gameObject.SetActive(false);
-    }
+		// using pooling
+		gameObject.SetActive(false);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		gameObject.SetActive(false);
+	}
 }
